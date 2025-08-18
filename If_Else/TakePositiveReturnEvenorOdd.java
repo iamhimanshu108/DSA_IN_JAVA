418cfeeca8da9d3cbcced36dd8519952914f014a
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2025-08-18 15:14:03 +0000

    Day3

---
+++ If_Else/TakePositiveReturnEvenorOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TakePositiveReturnEvenorOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Number");
+        int n = sc.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+    }
+}
