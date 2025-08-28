6e62d20f053421723675918b3e872b9f8c9b90b4
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2025-08-28 21:30:40 +0530

    Loops

---
+++ Loops/Loops.java
@@ -0,0 +1,9 @@
+public class Loops {
+    public static void main(String[] args) {
+       
+
+        for(int i=18; i<=20; i++){
+             System.out.println("Amir hates Nikita");
+        }
+    }
+}
