50b022477de108131a036b6ed0499b8a31f8f294
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2025-09-21 16:05:58 +0530

    Methods

---
+++ Methods/ReturnType.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class ReturnType {
+
+    public static int prasun(){
+        System.out.println("Banu");
+        return 0;
+    }
+    public static void main(String[] args) {
+        prasun();
+    }
+}
