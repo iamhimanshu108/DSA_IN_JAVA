fd13e99bdebf7aef6b272969081689dbf30989b3
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2025-10-05 09:15:34 +0530

    arraysList

---
+++ Arrays/ArrayListBasics.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class ArrayListBasics {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(25);
+        arr.add(21);
+        arr.add(18);
+        arr.add(5);
+        arr.add(10);
+
+        System.out.println(arr.get(2));
+    }
+}
