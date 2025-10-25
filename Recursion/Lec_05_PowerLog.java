c1ca24f7ffffb15c721ec5acd88ff6f1a5d0b31c
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2025-10-25 05:22:17 +0000

    Lec 05

---
+++ Recursion/Lec_05_PowerLog.java
@@ -0,0 +1,15 @@
+public class Lec_05_PowerLog {
+    public static void main(String[] args) {
+        
+    }
+
+
+    public static int power(int x, int n){
+        if(n==0){
+            return 1;
+
+        }
+        int xpnb2 = power(x,n/2);
+        
+    }
+}
